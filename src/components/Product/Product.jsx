c0625fe396cef91f9b6b2d83116588befc2869b3
parthooo <parthoo.rahman@gmil.com>
2023-04-01 01:42:38 +0600

    styling & add products git commit -m styling

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
 import "./Product.css";
 
 const Product = (props) => {
-    const{name, seller, price, ratings, img, quantity} = props.product
-    return (
-        <div className='card'>
-            <img src={img} alt="" />
-            <h6>{name}</h6>
-        </div>
-    );
+  const { name, seller, price, ratings, img, quantity } = props.product;
+  return (
+    <div className="card">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <h6 className="card-title">{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturar: {seller}</p>
+        <p>Ratings: {ratings} Stars</p>
+      </div>
+      <button className="btn-cart">Add to cart</button>
+    </div>
+  );
 };
 
 export default Product;
